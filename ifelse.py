b70371d9149eccb1c6b978e3a3f0dad0a6cb0e61
Vivesh123 <88177988+Vivesh123@users.noreply.github.com>
2022-07-23 16:46:53 +0530

    If else condition

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+age = int(input("Enter Your Age:?\n"))
+Name = input("Enter Your Name:?\n")
+if age>34:
+    print("The age of {} is {} and he is not a good man!".format(Name,age))
+elif 46<age>50:
+    print("The age of {} is {} and he is monoster!".format(Name,age))
+else:
+     print("The age of {} is {} and he is a very humble person!".format(Name,age))
